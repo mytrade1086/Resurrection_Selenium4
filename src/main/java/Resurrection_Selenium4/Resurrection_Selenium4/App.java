9c03bda58143835b091c81b880bd0a21d2b8ee10
Sumit <sumit@sumit.com>
2021-12-05 11:46:45 +0530

    first

---
+++ src/main/java/Resurrection_Selenium4/Resurrection_Selenium4/App.java
@@ -0,0 +1,13 @@
+package Resurrection_Selenium4.Resurrection_Selenium4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
